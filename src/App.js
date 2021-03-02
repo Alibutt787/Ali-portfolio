4d0f74964301a0f11b84392deeec1d2946314585
AQIB Ali Butt <Alibutt787.github.com>
2021-03-03 03:56:30 +0500

    comit

--- src/App.js
+++ src/App.js
@@ -6,27 +6,21 @@ import { Home } from './Home';
 import './App.css'
 import { Notfound } from './Notfound';
 import { Feedback } from './Feeback';
-import {  Covid } from './Covid';
-import {  Birthday } from './Birthday';
+import { Covid } from './Covid';
+import { Birthday } from './Birthday';
 
 
 
 function App() {
-
-
-
-
   return (
     <div  >
-
       <BrowserRouter>
-        <div className="con">
-
+        <div className="">
           <div className="  d-flex flex-row co  justify-content-between fixed-top" >
-            <div className="co" style={{ fontSize: "40px" }}><b>AQIB Portfolio</b><i class="fas fa-user-tie"></i></div>
-            <div className="dropdown co">
-              <button className="btn  " type="button" data-toggle="dropdown" style={{ width: "50px", height: "50px" }}>
-                <i className="fas fa-bars "  style={{fontSize: "2em"}}></i></button>
+            <div className="" style={{ fontSize: "40px" }}></div>
+            <div className="dropdown  ">
+              <button className="btn " type="button" data-toggle="dropdown" style={{ width: "50px", height: "50px" }}>
+                <i className="fas fa-bars " style={{ fontSize: "2em",color:"white"  }}></i></button>
               <ul className="  dropdown-menu  dropdown-menu-right "  >
                 <li className="  dropdown-item "> <Link className=" Link dropdown-item " to="/"><i className="fa fa-home "></i><span style={{ fontSize: "20px" }}>Home</span></Link> </li>
                 <li className="  dropdown-item "> <Link className="Link dropdown-item " to="/Videos"><i class="fas fa-video "></i><span style={{ fontSize: "20px" }}>Videos</span></Link></li>
@@ -34,16 +28,10 @@ function App() {
                 <li className="  dropdown-item "> <Link className="Link dropdown-item " to="/Covid"> <i class="fas fa-virus "></i> <span style={{ fontSize: "20px" }}>Covid_19</span></Link></li>
                 <li className="  dropdown-item "> <Link className="Link dropdown-item " to="/feedback"><i class="fas fa-comments "></i><span style={{ fontSize: "20px" }}>Feedback</span></Link></li>
                 <li className="  dropdown-item "> <Link className="Link dropdown-item " to="/contect"><i class="fas fa-address-book"></i><span style={{ fontSize: "20px" }}>Contect</span></Link></li>
-
-              
               </ul>
             </div>
-          
           </div>
-<br/>
-<br/>
-<br/>
-<br/>
+          
 
           <div className=" d-flex flex-row ">
             <Routes  >
@@ -51,19 +39,18 @@ function App() {
               <Route path="/Videos" element={<Videos />} />
               <Route path="/Covid" element={<Covid />} />
               <Route path="/Birthdays" element={<Birthday />} />
-            
-              <Route path="/Videos/:id" element={<About  />} />
+
+              <Route path="/Videos/:id" element={<About />} />
               <Route path="/feedback" element={<Feedback />} />
               <Route path="/*" element={<Notfound />} />
             </Routes>
 
           </div>
-
         </div>
       </BrowserRouter>
-      <nav class=" fixed-bottom navbar-light bg-dark bot" style={{textAlign:"center" ,color:"white"}} >
-  <div class=" bg-dark" ><h4><b>2020&copy;AliButt</b></h4></div>
-</nav>
+      <nav class=" fixed-bottom navbar-light bg-dark bot" style={{ textAlign: "center", color: "white" }} >
+        <div class=" bg-dark" ><h4><b>2020&copy;AliButt</b></h4></div>
+      </nav>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -4,31 +4,31 @@ export const Home = () => {
   return (
 
 
-    <div className="card  container  bg-dark ,mar" style={{ color: "white", width: "50rem", height: "400px", opacity: .7, margin: "0 auto" }}>
-    
+    <div className="card  container  bg-dark ,mar" style={{ color: "white", width: "50rem", height: "400px", opacity: .7, margin: "0 auto" ,marginTop:"70px" }}>
+
       <img src={aqib_pic} className=" card-img-top rounded-circle" style={{ marginTop: "10px", marginLeft: "28%", width: "40%", height: "40%" }} alt="Profile pic" />
-     
+
       <div className="card-body  " >
-        <div className ="  " style={{margin:"0 auto", marginLeft:"20%"}}>
-        <h3 className="card-title"><i class="fas fa-user"></i><b>  Aqib Ali Butt</b></h3>
-        <h5 className="card-text"><b> <i class="fas fa-university"></i>   Studying in Punjab University</b></h5>
-        <h5 className="card-text"><i class="fas fa-map-marker-alt"></i><b>  Live in Sialkot</b></h5>
-        <h5 className="card-text"><i class="far fa-calendar-alt"></i><b>  Wishing me on 10 Dec </b></h5>
-        <h5 className="card-text"><i class="fas fa-laptop-code"></i><b>  Learning phase</b></h5>
-        
-       </div>
-       <b className="card-text"><i class="fas fa-edit"></i> All limitations are self-imposed.</b>
-      
+        <div className="  " style={{ margin: "0 auto", marginLeft: "20%" }}>
+          <h3 className="card-title"><i class="fas fa-user"></i><b>  Aqib Ali Butt</b></h3>
+          <h5 className="card-text"><b> <i class="fas fa-university"></i>   Studying in Punjab University</b></h5>
+          <h5 className="card-text"><i class="fas fa-map-marker-alt"></i><b>  Live in Sialkot</b></h5>
+          <h5 className="card-text"><i class="far fa-calendar-alt"></i><b>  Wishing me on 10 Dec </b></h5>
+          <h5 className="card-text"><i class="fas fa-laptop-code"></i><b>  Learning phase</b></h5>
+
+        </div>
+        <b className="card-text"><i class="fas fa-edit"></i> All limitations are self-imposed.</b>
+
         <div className="d-flex justify-content-around b-0 ">
-       
+
           <h3 ><a href="https://twitter.com/AqibAliButt1"><i className="fab fa-twitter-square  "></i></a></h3>
           <h3><a href="https://www.instagram.com/aqibalibhat/?igshid=t0lwo233oilk"><i className="fab fa-instagram-square "></i></a></h3>
           <h3><a href="https://github.com/Alibutt787"> <i className="fab fa-github-square "></i></a></h3>
-          <h3><a  href="https://www.linkedin.com/in/aqib-ali-butt-a75086208/"><i className="fab fa-linkedin "></i></a></h3>
+          <h3><a href="https://www.linkedin.com/in/aqib-ali-butt-a75086208/"><i className="fab fa-linkedin "></i></a></h3>
 
-         
+
         </div>
-      
+
       </div>
     </div>
 
